d0b546412d0d257d43a538334cb179650672883f
Raul Sierra <j.raulsierra@gmail.com>
2023-12-08 08:10:52 -0600

    feat: adding logic and show process in creating a list of characters

--- src/app/dbz/components/list/list.component.ts
+++ src/app/dbz/components/list/list.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {Character} from "../../interfaces/character.interface";
 
 @Component({
   selector: 'dbz-list',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.css']
 })
-export class ListComponent implements OnInit {
+export class ListComponent {
 
-  constructor() { }
+  @Input()
+  public characterList: Character[] = [{
+    name: 'Trunks',
+    power: 10
+  }];
 
-  ngOnInit(): void {
+  // onDelete = Index value : number
+  @Output()
+  public onDelete: EventEmitter<string> = new EventEmitter();
+
+
+  onDeleteCharacter( id?: string ):void {
+
+    if ( !id ) return;
+    this.onDelete.emit( id );
   }
 
 }
